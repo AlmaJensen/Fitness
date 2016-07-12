c7a8ecd9abaf98874e9542f26609967f4c38695c
AlmaJensen <jazzdevstudio@gmail.com>
2016-07-11 21:03:24 -0600

    models added
    Added some data models

---
+++ FitnessGame/FitnessGame/DataModels/RewardModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessGame.DataModels
+{
+    public class RewardModel
+    {
+        public string RewardName { get; set; }
+        public int CashValue { get; set; }
+        public int RubyValue { get; set; }
+        public bool IsCollectionItem { get; set; }
+        public int Rarity { get; set; }
+    }
+}
---
+++ FitnessGame/FitnessGame/Services/StaticRewards.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessGame.Services
+{
+    public class StaticRewards
+    {
+        public static List<RewardModel> CommonRewards = new List<RewardModel>
+        {
+            RewardModel{},
+
+        };
+    }
+}
